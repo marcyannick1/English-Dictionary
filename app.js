1a9c82a1df7bf8b62146674dafa99d6ab441e992
Marc Yannick <102946646+marcyannick1@users.noreply.github.com>
2022-10-28 22:21:25 +0200

    audio add

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const form = document.querySelector('.form')
 const input = document.querySelector("#word")
 
 const word = document.querySelector('.word h2')
+const btnAudio = document.querySelector('.audio')
 const partOfSpeech = document.querySelector('.part-of-speech')
 const phoneticText = document.querySelector('.phonetic-text')
 
 const definition = document.querySelector('.definition')
 const example = document.querySelector('.example')
 
-const synonyms = document.querySelector('.synonyms')
-
 let inputWord = ""
+let audio = ""
 
 function firstLetterUC(word){
     wordUC = ''
@@ -24,17 +24,41 @@ function firstLetterUC(word){
 
 form.addEventListener('submit', (e)=>{
     inputWord = input.value
-    fetch(`${url} ${inputWord.replace(' ', '-')}`).then(
-        (response) => response.json().then(
-            (data) => {
-                word.textContent = firstLetterUC(data[0].word)
-                partOfSpeech.textContent = data[0].meanings[0].partOfSpeech
-                phoneticText.textContent = data[0].phonetic
-                definition.innerHTML = data[0].meanings[0].definitions[0].definition
-                example.textContent = data[0].meanings[0].definitions[0].example
-                console.log(data)
+    fetch(`${url}${inputWord.replace(' ', '-')}`).then((response) => response.json()
+        .then((data) => {
+            word.textContent = firstLetterUC(data[0].word)
+            let phonetics = data[0].phonetics
+
+            partOfSpeech.textContent = data[0].meanings[0].partOfSpeech
+            phoneticText.textContent = data[0].phonetic
+            definition.innerHTML = data[0].meanings[0].definitions[0].definition
+            example.textContent = data[0].meanings[0].definitions[0].example
+
+            if(phonetics.length == 0){
+                btnAudio.classList.add('hidden')
             }
-        )
-    );
+            for (const data of phonetics) {
+                if(data.audio != ''){
+                    btnAudio.classList.remove('hidden')
+                    audio = new Audio(data.audio)
+                    break
+                }else{
+                    btnAudio.classList.add('hidden')
+                }
+            }
+            console.log(data)
+        }).catch (() => {
+            word.textContent = "This word cannot be found"
+            btnAudio.classList.add('hidden')
+            partOfSpeech.textContent = ""
+            phoneticText.textContent = ""
+            definition.innerHTML = ""
+            example.textContent = ""
+        })
+    )
     e.preventDefault()
+})
+
+btnAudio.addEventListener('click',()=>{
+    audio.play()
 })
